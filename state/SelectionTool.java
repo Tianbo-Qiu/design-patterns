313cb1032a47a55b69847d50eb15a33514591340
Tianbo <tianboqiu@gmail.com>
2022-01-29 13:32:36 -0800

    State Pattern

---
+++ state/SelectionTool.java
@@ -0,0 +1,19 @@
+package state;
+
+/**
+ * Concrete implementation class.
+ */
+public class SelectionTool implements Tool {
+
+  @Override
+  public void mouseDown() {
+    System.out.println("Selection icon");
+  }
+
+  @Override
+  public void mouseUp() {
+    System.out.println("Draw a dashed rectangle");
+    
+  }
+
+}
